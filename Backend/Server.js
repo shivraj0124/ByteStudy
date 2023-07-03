12a88d667687213d86d02aff6777dee9597b65cf
Yash aghane <yashaghane21@gmail.com>
2023-07-03 21:31:43 +0530

    server making

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,5 +1,5 @@
-const express =require("express") 
-const app =express();
-app.listen(500,()=>{
-    console.log("Server is Running")
+const express=require("express")
+const app=express();
+app.listen(5000,()=>{
+    console.log("running")
 })
